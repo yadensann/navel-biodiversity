cbd5a796893ff2afb482539f2fc6035a91962682
oyasumi <yadensann@gmail.com>
2021-01-03 21:49:28 -0700

    still dont know what im doing

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,81 +1,124 @@
 // Pulling data from json file and adding them with filter
+
+d3.json('./data/samples.json', function(data) {
+    console.log(data);
+});
+
 function pullMetadata(sample) {
-    d3.json('data/samples.json').then((data) => {
-        // var metadata= data.metadata[0];
+    d3.json('./data/samples.json').then((response) => {
+        console.log(response);
+
+    // d3.json('./data/samples.json').then((data) => {
+        var data= response;
         var PANEL= d3.select('#sample-metadata');
         // var samplesArray = metadata.filter()
         PANEL.html("");
         // ary.filter => return a list (use [0])
         Object.entries(data).forEach(([key, value]) => {
-            PANEL.append("h6").text(`${key}: ${value}`);
+            PANEL.append("p").text(`${key}: ${value}`);
         });
     });
 }
 
-function buildCharts(sampleData, otuData) {
-    var labels= sampleData[0]['otu_ids'].map(function(item){
-        return otuData[item]
-    });
-    var bubbleLayout= {
-        margin: {t: 0},
-        hovermode:'closest',
-        xaxis: {title:'OTU ID'}
-    };
-    var bubbleData= [{
-        x: sampleData[0]['otu_ids'],
-        y: sampleData[0]['sample_values'],
-        text: labels,
-        mode: 'markers',
-        marker: {
-            size: sampleData[0]['sample_values'],
-            color: sampleData[0]['otu_ids'], 
-            colorscale: 'Earth'
-        }
-    }];
+function buildCharts(sample) {
+    d3.json('./data/samples.json').then((response => {
+        var xValue= response['']
+        var samples= data.samples;
+        var samplesArray= samples.filter(sampleObject => sampleObject.id == samples);
+        var result= samplesArray[0];
 
-    Plotly.plot('bubble', bubbleData, bubbleLayout);
 
-    var barChartData = [{
-        y: sampleData[0]['otu_ids'].slice(0,10).map(otuID => 'OTU ${otuID}').reverse(),
-        x: sampleData[0]['sample_values'].slice(0,10).reverse(),
-        text: labels.slice(0,10).reverse(),
-        type: 'bar',
-        orientation: 'h'
+    }));
 
-    }];
-    var barLayout= {
-        title:'Top 10 Bacteria Cultures Found',
-        margin: {t: 30, l: 150}
-    };
 
-    Plotly.newPlot('bar', barChartData, barLayout);
+        var ids= result.otu_ids;
+        var labels= result.otu_labels;
+        var values= result.sample_values;
+//     }))
+//     // var labels= sampleData[0]['otu_ids'].map(function(item){
+//     //     return otuData[item]
+//     };
+//     var bubbleLayout= {
+//         margin: {t: 0},
+//         hovermode:'closest',
+//         xaxis: {title:'OTU ID'}
+//     };
+//     var bubbleData= [{
+//         x: ids,
+//         y: values,
+//         text: labels,
+//         mode: 'markers',
+//         marker: {
+//             size: values,
+//             color: ids, 
+//             colorscale: 'Earth'
+//         }
+//     }];
 
-}    
+//     Plotly.plot('bubble', bubbleData, bubbleLayout);
 
-function init() {
-    var selector= d3.select('#selDataset');
+//     var barChartData = [{
+//         y: sampleData[0]['otu_ids'].slice(0,10).map(otuID => 'OTU ${otuID}').reverse(),
+//         x: sampleData[0]['sample_values'].slice(0,10).reverse(),
+//         text: labels.slice(0,10).reverse(),
+//         type: 'bar',
+//         orientation: 'h'
 
-    d3.json('data/samples.json').then((data) => {
-        var sampleNames = data.names;
-        sampleNames.forEach((sample)=>{
-            selector
-            .append('option')
-            .text(sample)
-            .property('value', sample);
-        });
+//     }];
+//     var barLayout= {
+//         title:'Top 10 Bacteria Cultures Found',
+//         margin: {t: 30, l: 150}
+//     };
 
-        const firstSample= sampleNames[0];
-        buildCharts(firstSample);
-        pullMetadata(firstSample);
-    });
+//     Plotly.newPlot('bar', barChartData, barLayout);
+
+// function init() {
+//     var selector= d3.select('#selDataset');
+
+
+//     // d3.json('./data/samples.json').then(function(data) 
+//     d3.json('data/samples.json').then(function(data) {
+
+//         console.log(data);
+
+//         var sampleNames = data.names;
+
+//         sampleNames.forEach((sample)=> {
+//             d3.select(selector)
+//             .data(data)
+//             .enter()
+//             .append('p')
+//             .text(sample)
+//             .property('value', sample);
+//         });
+
+//         const firstSample= sampleNames[0];
+//         buildCharts(firstSample);
+//         pullMetadata(firstSample);
+//     });
 }
 
-function dataChange(newSample) {
-    buildCharts(newSample);
-    pullMetadata(newSample);
-}
+//     d3.json('./data/samples.json').then((data) => {
+//         var sampleNames = data.names;
+//         sampleNames.forEach((sample)=>{
+//             selector
+//             .append('option')
+//             .text(sample)
+//             .property('value', sample);
+//         });
 
-init();
+//         const firstSample= sampleNames[0];
+//         buildCharts(firstSample);
+//         pullMetadata(firstSample);
+//     });
+// }
+
+// function dataChange(newSample) {
+//     buildCharts(newSample);
+//     pullMetadata(newSample);
+// }
+
+// init();
 
 
 
