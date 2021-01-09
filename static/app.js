3f1bc5aa664ab713c8371fccf97e3fa1d98be3a3
oyasumi <yadensann@gmail.com>
2021-01-08 23:29:24 -0700

    deleted other app.js file to replace with new one that works

--- static/app.js
+++ static/app.js
@@ -1,91 +1,156 @@
-// Pulling data from json file and adding them with filter
-
-// d3.json('/data', function(data) {
-//     console.log(data)
-// });
-var default_id='940';
-var sampleData;
-// function pullMetadata(sample){
-    d3.json('/data').then((response) => {
-        sampleData= response;
-        var metadata=response['metadata'];
-        console.log(metadata)
-        var just_one=metadata.filter(one=>one['id']==parseInt(default_id))[0]
-        console.log(just_one);
-        // var samplesArray= sampleData.filter(sampleobject => sampleobject.id == sample);
-        // var result= samplesArray[0];
-        // console.log(result);
-
-        var PANEL= d3.select('#sample-metadata');
-            console.log(PANEL);
-            PANEL.html("");
-            Object.entries(just_one).forEach(([key, value]) => {
-                PANEL.append("p").text(`${key}: ${value}`);
-                console.log([key, value]);
-            });
-    });  
-      
-    // function buildCharts(sample) {
-        var url= `/data`;
-        d3.json(url).then(response => {
-            //console.log(response);
-            var just_samples=response['samples'];
-            var xval= just_samples[];
-            var yval= just_samples.sample_values;
-
-            // var just_otu_ids=just_samples.filter(only=>only['otu_ids']);
-            console.log(just_samples);
-            // console.log(just_otu_ids);
-            console.log(xval);
-
-            // var trace={}
-            });
-            // console.log(Object.keys(otu_ids));
-
-
-        
-            // var otu_ids= ids[
-            // console.log(otu_ids);
-
+// function for dropdown menu change
+function optionChanged(selectedID){
+    console.log(selectedID);
+// read json file for data
+    d3.json("/data").then((data) => {
+   //  console.log(data);
+// clears dropdown
+    d3.select("#selDataset").html("");   
+// for loop for metadata array and appends the itemID to dropdown menu
+    data.metadata.forEach(item => {
+          // console.log(item.id);
+         d3.select ("#selDataset").append('option').attr('value', item.id).text(item.id);
+         });
+// selected value equals selectedID function
+    d3.select("#selDataset").node().value = selectedID;
+// filter metadata for selectedID from dropdown
+    const metadataID = data.metadata.filter(item=> (item.id == selectedID));
+    console.log(metadataID);
     
-        // var labels= result.otu_labels;
-        // var values= result.sample_values;
+    const PANEL = d3.select("#sample-metadata");
+    PANEL.html("");
+    Object.entries(metadataID[0]).forEach(item=> 
+       {
+          // console.log(item);
+          PANEL.append("p").text(`${item[0]}: ${item[1]}`)
+       });
+ 
+// BAR CHART
+    // filter samples data for otu_ids
+    const sampleID = data.samples.filter(item => parseInt(item.id) == selectedID);
+    // console.log(sampleID[0].sample_values);  
+    // console.log(sampleID[0].otu_ids);  
+    // console.log(sampleID[0].otu_labels);  
 
-    //     var bubbleLayout= {
-    //         margin: {t: 0},
-    //         hovermode:'closest',
-    //         xaxis: {title:'OTU IDs'}
-    //         };
-    //     var bubbleData= [{
-    //         x: ids,
-    //         y: values,
-    //         text: labels,
-    //         mode: 'markers',
-    //         marker: {
-    //             size: values,
-    //             color: ids, 
-    //             colorscale: 'Earth'
-    //         }
-    //     }];
+    // slice top 10 values
+    var sample_value = sampleID[0].sample_values.slice(0,10);
+    sample_value= sample_value.reverse();
     
-    //     Plotly.plot('bubble', bubbleData, bubbleLayout);
+    var otu_id = sampleID[0].otu_ids.slice(0,10);
+    otu_id = otu_id.reverse();
+    
+    var otu_labels = sampleID[0].otu_labels
+    otu_labels = otu_labels.reverse();
+
+    //  console.log(sample_values);
+    //  console.log(otu_id);
+    //  console.log(otu_labels);
+ 
+    // y-axis of bar chart
+    const y_axis = otu_id.map(item => 'OTU' + " " + item);
+       // console.log(y-xis);
+    
+    const bar_trace = {
+       y: y_axis,
+       x: sample_value,
+       type: 'bar',
+       orientation: "h",
+       text:  otu_labels,
+        marker: {
+            color: 'peru',
+            line: {width: 3}
+        }
+    },
+       bar_layout = {
+       title: 'Top 10 Operational Taxonomic Units/Individuals',
+       xaxis: {title: 'Number of Samples Collected'},
+       yaxis: {title: 'OTU ID'}
+       };
+ 
+    // bar plot
+       Plotly.newPlot('bar', [bar_trace], bar_layout,  {responsive: true});    
+       
+ // BUBBLE CHART
+ 
+    // use only sample_value and otu_id for bubble chart
+    var only_sample_values =sampleID[0].sample_values;
+    var only_otu_ids= sampleID[0].otu_ids;
+
+    // Define the layout and trace object, edit color and orientation
+    const bubble_trace = {
+        x: only_otu_ids,
+        y: only_sample_values,
+        mode: 'markers',
+        marker: {
+            color: only_otu_ids,
+            size: only_sample_values
+        }
+    },
+    bubble_layout = {
+        title: '<b>Number of Samples vs. Operational Taxonomic Unit</b>',
+        xaxis: {title: 'OTU ID'},
+        yaxis: {title: 'Number of Samples Collected'},
+        showlegend: false,
+        height: 800,
+        width: 1500
+        };
+
+        // plot bubble chart
+    Plotly.newPlot('bubble', [bubble_trace], bubble_layout);
+    
 
 
+    // BONUS: GAUGE CHART
+        // gauge chart for weekly washing frequency 
+    const gauge_display = d3.select("#gauge");
+    gauge_display.html(""); 
+    const wash_freq = metadataID[0].wfreq;
+    
+    const gauge_data = [{
+        domain: {x:[0, 1], y:[0, 1]},
+        value: wash_freq,
+        title: {text: "<b>Belly Button Washing Frequency </b><br> (Scrubs Per Week)"},
+        type: "indicator",
+        mode: "gauge+number",     
+        gauge: {
+            axis: {range:[0,10]},
+                //scrub indicator on gauge chart
+                bar: {color: "#556B2F"},
+                // 0 to 10
+                steps: [
+                    {range: [0, 1], color: "white"},
+                    {range: [1, 2], color: "whitesmoke"},
+                    {range: [2, 3], color: "#d2b9b4"},
+                    {range: [3, 4], color: "#c9ada7"},
+                    {range: [4, 5], color: "#ac9899"},
+                    {range: [5, 6], color: "#8a7e88"},
+                    {range: [6, 7], color: "#7d7482"},
+                    {range: [7, 8], color: "#706a7b"},
+                    {range: [8, 9], color: "#4a4e69"},
+                    {range: [9, 10], color: "black"}    
+                ],
+                threshold: {
+                    value: wash_freq
+                }
+            }
+        }]; 
 
+    const gauge_layout = {  
+        width: 600, 
+        height: 400, 
+        margin: { t: 0, b: 0 }, 
+        };
 
-        // var trace1= {
-        //     x: x_value,
-        //     y: y_value,
-        //     mode:'markers',
-        //     marker:{
-        //         size: sizeValue,
-        //         color: xValue,
-        //         colorscale: 'Earth',
-        //         labels: label,
-        //         type: 'scatter',
-
-        //     }
-        // };
+        // plot gauge chart
+    Plotly.newPlot('gauge', gauge_data, gauge_layout); 
+    });
+}
+     // data starts at ID 940
+optionChanged(940);
+// event on change takes value and calls the function during dropdown selection
+d3.select("#selDataset").on('change',() => {
+    optionChanged(d3.event.target.value);
+});
 
 
 
